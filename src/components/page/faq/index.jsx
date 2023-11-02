f2e716b94d5b2b6e81850ae9ef6c2e927ddef6ca
Naufal Zufar S <naufal.zufar.s@gmail.com>
2023-11-02 08:54:17 +0700

    feat: create FAQ page

--- src/components/page/faq/index.jsx
+++ src/components/page/faq/index.jsx
@@ -1,5 +1,64 @@
+import { tokens } from "@/src/theme";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "../../common/Header";
+import { ExpandMore } from "@mui/icons-material";
+
 export default function FAQ() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <div>FAQ</div>
-  )
+    <Box>
+      <Header title="FAQ" subtitle="Frequently Asked Question" />
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography variant="h5" color={colors.greenAccent[500]}>
+            How to Add New Users
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            First go to &#34;New User&#34; Page, then fill all the field, then
+            click the submit button
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography variant="h5" color={colors.greenAccent[500]}>
+            Add New Event into Calendar
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Go to &#34;Calendar&#34; Page, click on the date you want to add
+            event. Title prompt will appear, fill the title field and choose
+            &#34;OK&#34;.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography variant="h5" color={colors.greenAccent[500]}>
+            See Member Access Level
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            You can see other member access level on &#34;Manage Team&#34; page
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
 }
